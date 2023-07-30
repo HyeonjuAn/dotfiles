36974628aabfcd723add7249946347f127d32411
Johnny An <anhyeonju35@gmail.com>
2023-07-29 21:39:20 -0500

    treesitter-context

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -204,6 +204,11 @@ _G.packer_plugins = {
     path = "/Users/hyeonjuan/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
+  ["nvim-treesitter-context"] = {
+    loaded = true,
+    path = "/Users/hyeonjuan/.local/share/nvim/site/pack/packer/start/nvim-treesitter-context",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter-context"
+  },
   ["nvim-ts-autotag"] = {
     loaded = true,
     path = "/Users/hyeonjuan/.local/share/nvim/site/pack/packer/start/nvim-ts-autotag",
