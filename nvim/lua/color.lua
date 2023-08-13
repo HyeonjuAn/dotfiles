f93bada14daedfb2bff343ce2e5db89f336865e1
Johnny An <anhyeonju35@gmail.com>
2023-08-13 17:05:44 -0500

    Gruvbox dark background

--- nvim/lua/color.lua
+++ nvim/lua/color.lua
@@ -1,6 +1,8 @@
 vim.g.gruvbox_material_diagnostic_line_highlight = 1
 vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
 vim.g.gruvbox_material_diagnostic_text_highlight = 1
+vim.g.gruvbox_material_background = 'hard'
+vim.g.gruvbox_material_foreground = 'material'
 vim.cmd('colorscheme gruvbox-material')
 
 -- vim.g.everforest_diagnostic_line_highlight = 1
