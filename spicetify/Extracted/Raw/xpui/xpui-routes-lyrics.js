f0f0e39fb86e534d0f9533bcee8bdfa03b966228
Johnny An <anhyeonju35@gmail.com>
2024-03-18 14:59:41 -0500

    minor changes

--- spicetify/Extracted/Raw/xpui/xpui-routes-lyrics.js
+++ spicetify/Extracted/Raw/xpui/xpui-routes-lyrics.js
@@ -1,1 +1,1 @@
-"use strict";(("undefined"!=typeof self?self:global).webpackChunkopen=("undefined"!=typeof self?self:global).webpackChunkopen||[]).push([[6025],{52687:(e,l,f)=>{f.r(l),f.d(l,{default:()=>n.Z});var n=f(34924)}}]);
+"use strict";(("undefined"!=typeof self?self:global).webpackChunkopen=("undefined"!=typeof self?self:global).webpackChunkopen||[]).push([[6151],{88436:(e,l,f)=>{f.r(l),f.d(l,{default:()=>n.c});var n=f(75516)}}]);
